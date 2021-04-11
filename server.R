db0d9e9d4a5c623cacac547f3623ce5ba8313df3
Patrice Roux <patrice.roux@gmail.com>
2021-04-11 17:41:50 +0200

    Data cleaning - 2017
    Ajout d'un csv sur les coord 2017 en erreur plus filtrage dans le serveur Shiny.
    Tentative de géocodage inversé avec https://geo.api.gouv.fr/adresse (non concluant pour le moment)

--- server.R
+++ server.R
@@ -12,7 +12,51 @@ library(DT)
 library(tigris)
 library(leaflet)
 library(tidyverse)
+library(jsonlite)
 
+
+#Annee 2017####
+caracteristiques.2017 <- read.csv2("data/caracteristiques-2017.csv", sep=',', stringsAsFactors=TRUE)
+# Départements
+caracteristiques.2017 <- caracteristiques.2017 %>% mutate(dep = case_when(dep == 201 ~ "2A",
+                                                                          dep == 202 ~ "2B",
+                                                                          dep == 971 ~ "971",
+                                                                          dep == 972 ~ "972",
+                                                                          dep == 973 ~ "973",
+                                                                          dep == 975 ~ "975",
+                                                                          dep == 974 ~ "974",
+                                                                          dep == 976 ~ "976",
+                                                                          dep == 977 ~ "977",
+                                                                          dep == 978 ~ "978",
+                                                                          dep == 987 ~ "987",
+                                                                          dep == 986 ~ "986",
+                                                                          dep == 988 ~ "988",
+                                                                          TRUE ~ as.character(dep/10)))
+caracteristiques.2017 <- caracteristiques.2017 %>%
+    mutate(lat = lat / 100000) %>% 
+    mutate(long = long / 100000) %>%
+    mutate(an = 2017)
+# Inversion des signes pour lattitudes et longitudes pour les départements 971, 972, 973, 974, 976
+caracteristiques.2017 <- caracteristiques.2017 %>% mutate(lat = case_when(dep %in% c(976,974) ~ -lat, TRUE ~ lat))
+caracteristiques.2017 <- caracteristiques.2017 %>% mutate(long = case_when(dep %in% c(972,973,971) ~ -long, TRUE ~ long))
+
+# Anomalie dans le département 92. Des coordonnées arbitraires
+caracteristiques.2017 <- caracteristiques.2017 %>% filter(!(dep == 92 & long == 2.2 & lat == 48))
+
+# Valeur de GPS non concordantes au département
+carac.2017.erreur <- read.csv2("data/coord_erreur_2017.csv", sep=';', stringsAsFactors=TRUE)
+caracteristiques.2017.filtre <- caracteristiques.2017 %>% filter(!Num_Acc %in% carac.2017.erreur$coordonnes)
+
+# Utilisation d'api de géocodage pour vérifier les coordonnées GPS par rapport au département
+# https://thinkr.fr/les-api-un-enfer/
+get_poscode_by_lat_long <- function(lat, long){
+    url <- paste("https://api-adresse.data.gouv.fr/reverse/?lon=",long, "&lat=",lat, sep = "")
+    result <- fromJSON(url)
+    return(result$features$properties$postcode)
+}
+result <- get_poscode_by_lat_long(49.62469, 1.61713)
+
+# caracteristiques.2017.filtre <- caracteristiques.2017 %>% filter(lat) mutate(postcode = get_poscode_by_lat_long(lat, long))
 
 #Annee 2018####
 # Data 
@@ -40,6 +84,8 @@ caracteristiques.2018 <- caracteristiques.2018 %>%
 # Inversion des signes pour lattitudes et longitudes pour les départements 971, 972, 973, 974, 976
 caracteristiques.2018 <- caracteristiques.2018 %>% mutate(lat = case_when(dep %in% c(976,974) ~ -lat, TRUE ~ lat))
 caracteristiques.2018 <- caracteristiques.2018 %>% mutate(long = case_when(dep %in% c(972,973,971) ~ -long, TRUE ~ long))
+# Tous les accidents du département 972 ayant -60 en long sont éronnées au niveau de leur coordonnées GPS.
+caracteristiques.2018 <- caracteristiques.2018 %>% filter(!(dep == 972 & long == 60))
 
 # Valeur de GPS non concordantes au département
 carac.2018.erreur <- read.csv2("data/coord_erreur_2018.csv", sep=';', stringsAsFactors=TRUE)
@@ -47,16 +93,15 @@ caracteristiques.2018.filtre <- caracteristiques.2018 %>% filter(!Num_Acc %in% c
 
 
 #Annee 2019####
-
 caracteristiques.2019 <- read.csv2("data/caracteristiques-2019.csv", stringsAsFactors=TRUE)
 # Nettoyage des accidents dont les coordonnées GPS ne correspondent pas. Annee 2019
 carac.erronnes <- c(201900033874, 201900035540, 201900035541, 201900007105, 201900058723, 201900017468, 201900005927, 201900033236, 201900006562
                     , 201900056421, 201900000632, 201900057241, 201900048863, 201900008746, 201900056494, 201900047454, 201900021441, 201900025903
                     ,201900020406)
-
-# Selection des données
-# caracteristiques.2019.filtre <- caracteristiques.2018
 caracteristiques.2019.filtre <- caracteristiques.2019 %>% filter(!Num_Acc %in% carac.erronnes)
+
+# Selection des données de l'année. A retravailler pour faire une sélection groupée / fusionnée.
+caracteristiques.filtre <- caracteristiques.2017.filtre
 
 # Server Logic
 
@@ -79,18 +124,15 @@ server <- function(input, output, session) {
     output$plot_comparison_indicator_between_states <- renderPlot({})
     
     # Selection du département à afficher sur la cart
-    caracteristiques.2018.select <- reactive({
-        caracteristiques.2018.filtre %>% filter(dep == input$dep_fr_map)
+    caracteristiques.select <- reactive({
+        caracteristiques.filtre %>% filter(dep == input$dep_fr_map)
     })
-    caracteristiques.2019.select <- reactive({
-        caracteristiques.2019.filtre %>% filter(dep == input$dep_fr_map)
-        })
     
     output$fr_map <- renderLeaflet({
         # isolate(caracteristiques.2019.select)
         leaflet() %>%
             addTiles() %>%
-            addMarkers(lng=(caracteristiques.2018.select())$long, lat=(caracteristiques.2018.select())$lat , popup=(paste0(caracteristiques.2018.select()$Num_Acc)))
+            addMarkers(lng=(caracteristiques.select())$long, lat=(caracteristiques.select())$lat , popup=(paste0(caracteristiques.select()$Num_Acc)))
             })    
     
 }
--- testGeocodage.R
+++ testGeocodage.R
@@ -1,4 +1,5 @@
 library(httr)
+library(tidyverse)
 
 getDepartement_by_long_lat <- function(long, lat){
   url <- paste("https://api-adresse.data.gouv.fr/reverse/?lon=",long, "&lat=",lat, sep = "")
@@ -9,7 +10,7 @@ getDepartement_by_long_lat(2.37,48.357)
 
 
 # récupération des long et lat pour le géocodage inversé de masse
-caracteristiques.2018 %>% select(lat,long) %>% readr::write_delim(file = "data/file2018.csv", delim = ",")
+caracteristiques.2018 %>% select(lat, long) %>% rename (lon = long) %>% readr::write_delim(file = "data/file2018.csv", delim = ",")
 
 #curl -X POST -F data=@path/to/file.csv https://api-adresse.data.gouv.fr/reverse/csv/
   
@@ -17,13 +18,17 @@ getGeocodage <- function(file){
     tmp <- tempfile()
     h <- new_handle()
     handle_setform(h,
-                   data=form_file("data/file2018.csv", "@path/file2018.csv"))
+                   data=form_file("data/file2018.csv", "@/file2018.csv"))
+    handle_setopt(h, TIMEOUT = 120)
     curl_download("https://api-adresse.data.gouv.fr/reverse/csv/",tmp, handle = h)
     readLines(tmp)
     
   }
 getDepartement_by_long_lat(2.37,48.357)
 
+geocode.2018.1 <- readr::read_delim(file = "data/2018_1-20000.csv", delim = ",")
+geocode.2018.2 <- readr::read_delim(file = "data/2018_20001-40000.csv", delim = ",")
+geocode.2018.3 <- readr::read_delim(file = "data/2018_40001", delim = ",")
 #REF
 # https://pasq.fr/geocodage-inverse-grace-a-la-ban
 # http://gis.19327.n8.nabble.com/Geocodage-inverse-d-un-csv-avec-https-adresse-data-gouv-fr-api-td5941260.html
